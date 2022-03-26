758e10182fa7b9587d4f4c61151d740db6397173
Ebru Rice <ebruerenb@gmail.com>
2022-03-26 12:58:34 -0700

    Add models files and code

---
+++ Factory/Models/EngineerMachine.cs
@@ -0,0 +1,11 @@
+namespace Factory.Models
+{
+  public class EngineerMachine
+  {
+    public int EngineerMachineId { get; set; }
+    public int EngineerId { get; set; }
+    public int MachineId { get; set; }
+    public virtual Engineer engineer { get; set; }
+    public virtual Machine machine { get; set; }
+  }
+}
